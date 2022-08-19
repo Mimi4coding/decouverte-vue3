153fe59a3c9af31b8c608f13d727675431ac758a
Mimi4coding <111451030+Mimi4coding@users.noreply.github.com>
2022-08-19 22:59:57 +0200

    Add files via upload

---
+++ components/tab.vue
@@ -0,0 +1,42 @@
+<script>
+import DemoGrid from './Grid.vue'
+
+export default {
+  components: {
+    DemoGrid
+  },
+  data: () => ({
+    searchQuery: '',
+    gridColumns: ['CoreUI'],
+    gridData: [
+      {  CoreUI: 'Dashboard'},
+      {  CoreUI: '---THEME' },
+      {  CoreUI: 'Colors' },
+      {  CoreUI: 'Typgraphy'},
+      {  CoreUI: '---COMPONENTS'},
+      {  CoreUI: 'Base' },
+      {  CoreUI: 'Buttons' },
+      {  CoreUI: 'Charts' },
+      {  CoreUI: 'Forms' },
+      {  CoreUI: 'Icons' },
+      {  CoreUI: 'Notifications' },
+      {  CoreUI: 'Widgets (new)' },
+      {  CoreUI: '---EXTRAS' },
+      {  CoreUI: 'Pages' },
+      {  CoreUI: 'Docs' },
+      {  CoreUI: 'Try CoreUIPRO' },
+    ]
+  })
+}
+</script>
+
+<template>
+  <form id="search">
+    Search <input name="query" v-model="searchQuery">
+  </form>
+  <DemoGrid
+    :data="gridData"
+    :columns="gridColumns"
+    :filter-key="searchQuery">
+  </DemoGrid>
+</template>
